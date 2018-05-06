d1511364f47ba8a7cbde9cdd50b9ecda9a1a3389
Adam Doupe <adamdoupe@gmail.com>
2018-05-05 17:19:02 -0700

    Adding the library that I used to create the extension, php-cpp

---
+++ src/PHP-CPP/static/zend/callable.d
@@ -0,0 +1,290 @@
+static/zend/callable.o: zend/callable.cpp /usr/include/stdc-predef.h \
+ zend/includes.h /usr/include/stdlib.h /usr/include/features.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h /usr/include/xlocale.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/c++/5/string \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/os_defines.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/cpu_defines.h \
+ /usr/include/c++/5/bits/stringfwd.h /usr/include/c++/5/bits/memoryfwd.h \
+ /usr/include/c++/5/bits/char_traits.h \
+ /usr/include/c++/5/bits/stl_algobase.h \
+ /usr/include/c++/5/bits/functexcept.h \
+ /usr/include/c++/5/bits/exception_defines.h \
+ /usr/include/c++/5/bits/cpp_type_traits.h \
+ /usr/include/c++/5/ext/type_traits.h \
+ /usr/include/c++/5/ext/numeric_traits.h \
+ /usr/include/c++/5/bits/stl_pair.h /usr/include/c++/5/bits/move.h \
+ /usr/include/c++/5/bits/concept_check.h /usr/include/c++/5/type_traits \
+ /usr/include/c++/5/bits/stl_iterator_base_types.h \
+ /usr/include/c++/5/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/5/debug/debug.h /usr/include/c++/5/bits/stl_iterator.h \
+ /usr/include/c++/5/bits/ptr_traits.h \
+ /usr/include/c++/5/bits/predefined_ops.h \
+ /usr/include/c++/5/bits/postypes.h /usr/include/c++/5/cwchar \
+ /usr/include/wchar.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/c++/5/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stdint.h /usr/include/stdint.h \
+ /usr/include/c++/5/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/c++allocator.h \
+ /usr/include/c++/5/ext/new_allocator.h /usr/include/c++/5/new \
+ /usr/include/c++/5/exception \
+ /usr/include/c++/5/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/5/bits/exception_ptr.h \
+ /usr/include/c++/5/bits/nested_exception.h \
+ /usr/include/c++/5/bits/localefwd.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/c++locale.h \
+ /usr/include/c++/5/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/5/iosfwd \
+ /usr/include/c++/5/cctype /usr/include/ctype.h \
+ /usr/include/c++/5/bits/ostream_insert.h \
+ /usr/include/c++/5/bits/cxxabi_forced.h \
+ /usr/include/c++/5/bits/stl_function.h \
+ /usr/include/c++/5/backward/binders.h \
+ /usr/include/c++/5/bits/range_access.h \
+ /usr/include/c++/5/initializer_list \
+ /usr/include/c++/5/bits/basic_string.h \
+ /usr/include/c++/5/ext/atomicity.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/gthr.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/atomic_word.h \
+ /usr/include/c++/5/ext/alloc_traits.h \
+ /usr/include/c++/5/bits/alloc_traits.h \
+ /usr/include/c++/5/ext/string_conversions.h /usr/include/c++/5/cstdlib \
+ /usr/include/c++/5/cstdio /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/c++/5/cerrno /usr/include/errno.h \
+ /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/c++/5/bits/functional_hash.h \
+ /usr/include/c++/5/bits/hash_bytes.h \
+ /usr/include/c++/5/bits/basic_string.tcc /usr/include/c++/5/vector \
+ /usr/include/c++/5/bits/stl_construct.h \
+ /usr/include/c++/5/bits/stl_uninitialized.h \
+ /usr/include/c++/5/bits/stl_vector.h \
+ /usr/include/c++/5/bits/stl_bvector.h /usr/include/c++/5/bits/vector.tcc \
+ /usr/include/c++/5/map /usr/include/c++/5/bits/stl_tree.h \
+ /usr/include/c++/5/ext/aligned_buffer.h \
+ /usr/include/c++/5/bits/stl_map.h /usr/include/c++/5/tuple \
+ /usr/include/c++/5/utility /usr/include/c++/5/bits/stl_relops.h \
+ /usr/include/c++/5/array /usr/include/c++/5/stdexcept \
+ /usr/include/c++/5/bits/uses_allocator.h \
+ /usr/include/c++/5/bits/stl_multimap.h /usr/include/c++/5/set \
+ /usr/include/c++/5/bits/stl_set.h /usr/include/c++/5/bits/stl_multiset.h \
+ /usr/include/c++/5/memory /usr/include/c++/5/bits/stl_tempbuf.h \
+ /usr/include/c++/5/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/5/typeinfo /usr/include/c++/5/ext/concurrence.h \
+ /usr/include/c++/5/functional /usr/include/c++/5/bits/unique_ptr.h \
+ /usr/include/c++/5/bits/shared_ptr.h \
+ /usr/include/c++/5/bits/shared_ptr_base.h \
+ /usr/include/c++/5/bits/allocated_ptr.h \
+ /usr/include/c++/5/bits/shared_ptr_atomic.h \
+ /usr/include/c++/5/bits/atomic_base.h \
+ /usr/include/c++/5/backward/auto_ptr.h /usr/include/c++/5/list \
+ /usr/include/c++/5/bits/stl_list.h /usr/include/c++/5/bits/list.tcc \
+ /usr/include/c++/5/iostream /usr/include/c++/5/ostream \
+ /usr/include/c++/5/ios /usr/include/c++/5/bits/ios_base.h \
+ /usr/include/c++/5/bits/locale_classes.h \
+ /usr/include/c++/5/bits/locale_classes.tcc \
+ /usr/include/c++/5/system_error \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/error_constants.h \
+ /usr/include/c++/5/streambuf /usr/include/c++/5/bits/streambuf.tcc \
+ /usr/include/c++/5/bits/basic_ios.h \
+ /usr/include/c++/5/bits/locale_facets.h /usr/include/c++/5/cwctype \
+ /usr/include/wctype.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/ctype_base.h \
+ /usr/include/c++/5/bits/streambuf_iterator.h \
+ /usr/include/x86_64-linux-gnu/c++/5/bits/ctype_inline.h \
+ /usr/include/c++/5/bits/locale_facets.tcc \
+ /usr/include/c++/5/bits/basic_ios.tcc \
+ /usr/include/c++/5/bits/ostream.tcc /usr/include/c++/5/istream \
+ /usr/include/c++/5/bits/istream.tcc /usr/include/php/20151012/main/php.h \
+ /usr/include/php/20151012/main/php_config.h /usr/include/string.h \
+ /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_val.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
+ /usr/include/x86_64-linux-gnu/bits/inf.h \
+ /usr/include/x86_64-linux-gnu/bits/nan.h \
+ /usr/include/x86_64-linux-gnu/bits/mathdef.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/php/20151012/main/php_version.h \
+ /usr/include/php/20151012/Zend/zend.h \
+ /usr/include/php/20151012/Zend/zend_types.h \
+ /usr/include/php/20151012/Zend/zend_portability.h \
+ /usr/include/php/20151012/Zend/zend_config.h \
+ /usr/include/php/20151012/main/../main/php_config.h \
+ /usr/include/php/20151012/Zend/../TSRM/TSRM.h \
+ /usr/include/php/20151012/TSRM/tsrm_config.h \
+ /usr/include/php/20151012/main/php_stdint.h /usr/include/inttypes.h \
+ /usr/include/assert.h /usr/include/dlfcn.h \
+ /usr/include/x86_64-linux-gnu/bits/dlfcn.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/php/20151012/Zend/zend_range_check.h \
+ /usr/include/php/20151012/Zend/zend_long.h \
+ /usr/include/php/20151012/Zend/zend_errors.h \
+ /usr/include/php/20151012/Zend/zend_alloc.h \
+ /usr/include/php/20151012/Zend/zend.h \
+ /usr/include/php/20151012/Zend/zend_alloc_sizes.h \
+ /usr/include/php/20151012/Zend/zend_llist.h \
+ /usr/include/php/20151012/Zend/zend_string.h \
+ /usr/include/php/20151012/Zend/zend_hash.h \
+ /usr/include/php/20151012/Zend/zend_ast.h \
+ /usr/include/php/20151012/Zend/zend_gc.h \
+ /usr/include/php/20151012/Zend/zend_variables.h \
+ /usr/include/php/20151012/Zend/zend_iterators.h \
+ /usr/include/php/20151012/Zend/zend_stream.h \
+ /usr/include/x86_64-linux-gnu/sys/stat.h \
+ /usr/include/x86_64-linux-gnu/bits/stat.h \
+ /usr/include/php/20151012/Zend/zend_object_handlers.h \
+ /usr/include/php/20151012/Zend/zend_operators.h \
+ /usr/include/php/20151012/Zend/zend_strtod.h \
+ /usr/include/php/20151012/Zend/zend_multiply.h \
+ /usr/include/php/20151012/Zend/zend_sort.h \
+ /usr/include/php/20151012/main/php_compat.h \
+ /usr/include/php/20151012/main/php_config.h \
+ /usr/include/php/20151012/Zend/zend_API.h \
+ /usr/include/php/20151012/Zend/zend_modules.h \
+ /usr/include/php/20151012/Zend/zend_compile.h \
+ /usr/include/php/20151012/Zend/zend_globals.h /usr/include/setjmp.h \
+ /usr/include/php/20151012/Zend/zend_globals_macros.h \
+ /usr/include/php/20151012/Zend/zend_stack.h \
+ /usr/include/php/20151012/Zend/zend_ptr_stack.h \
+ /usr/include/php/20151012/Zend/zend_objects.h \
+ /usr/include/php/20151012/Zend/zend_objects_API.h \
+ /usr/include/php/20151012/Zend/zend_float.h \
+ /usr/include/x86_64-linux-gnu/fpu_control.h \
+ /usr/include/php/20151012/Zend/zend_multibyte.h \
+ /usr/include/php/20151012/Zend/zend_arena.h \
+ /usr/include/php/20151012/Zend/zend_vm_opcodes.h \
+ /usr/include/php/20151012/Zend/zend_build.h \
+ /usr/include/php/20151012/Zend/zend_list.h \
+ /usr/include/php/20151012/Zend/zend_execute.h \
+ /usr/include/php/20151012/main/build-defs.h /usr/include/unistd.h \
+ /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h /usr/include/getopt.h \
+ /usr/include/php/20151012/main/php_stdint.h \
+ /usr/include/php/20151012/Zend/zend_hash.h \
+ /usr/include/php/20151012/Zend/zend_alloc.h \
+ /usr/include/php/20151012/Zend/zend_stack.h /usr/include/pwd.h \
+ /usr/include/x86_64-linux-gnu/sys/param.h /usr/include/signal.h \
+ /usr/include/x86_64-linux-gnu/bits/signum.h \
+ /usr/include/x86_64-linux-gnu/bits/siginfo.h \
+ /usr/include/x86_64-linux-gnu/bits/sigaction.h \
+ /usr/include/x86_64-linux-gnu/bits/sigcontext.h \
+ /usr/include/x86_64-linux-gnu/bits/sigstack.h \
+ /usr/include/x86_64-linux-gnu/sys/ucontext.h \
+ /usr/include/x86_64-linux-gnu/bits/sigthread.h \
+ /usr/include/x86_64-linux-gnu/bits/param.h /usr/include/linux/param.h \
+ /usr/include/x86_64-linux-gnu/asm/param.h \
+ /usr/include/asm-generic/param.h \
+ /usr/include/php/20151012/main/snprintf.h \
+ /usr/include/php/20151012/main/spprintf.h \
+ /usr/include/php/20151012/main/php_output.h \
+ /usr/include/php/20151012/main/php_streams.h \
+ /usr/include/x86_64-linux-gnu/sys/time.h \
+ /usr/include/php/20151012/Zend/zend_stream.h \
+ /usr/include/php/20151012/main/streams/php_stream_context.h \
+ /usr/include/php/20151012/main/streams/php_stream_filter_api.h \
+ /usr/include/php/20151012/main/streams/php_stream_transport.h \
+ /usr/include/x86_64-linux-gnu/sys/socket.h \
+ /usr/include/x86_64-linux-gnu/sys/uio.h \
+ /usr/include/x86_64-linux-gnu/bits/uio.h \
+ /usr/include/x86_64-linux-gnu/bits/socket.h \
+ /usr/include/x86_64-linux-gnu/bits/socket_type.h \
+ /usr/include/x86_64-linux-gnu/bits/sockaddr.h \
+ /usr/include/x86_64-linux-gnu/asm/socket.h \
+ /usr/include/asm-generic/socket.h \
+ /usr/include/x86_64-linux-gnu/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h \
+ /usr/include/php/20151012/main/streams/php_stream_plain_wrapper.h \
+ /usr/include/php/20151012/main/streams/php_stream_glob_wrapper.h \
+ /usr/include/php/20151012/main/streams/php_stream_userspace.h \
+ /usr/include/php/20151012/main/streams/php_stream_mmap.h \
+ /usr/include/php/20151012/main/php_memory_streams.h \
+ /usr/include/php/20151012/main/fopen_wrappers.h \
+ /usr/include/php/20151012/main/php_globals.h \
+ /usr/include/php/20151012/Zend/zend_globals.h \
+ /usr/include/php/20151012/main/php_ini.h \
+ /usr/include/php/20151012/Zend/zend_ini.h \
+ /usr/include/php/20151012/Zend/zend_virtual_cwd.h \
+ /usr/include/php/20151012/TSRM/TSRM.h \
+ /usr/include/php/20151012/TSRM/tsrm_config_common.h /usr/include/utime.h \
+ /usr/include/dirent.h /usr/include/x86_64-linux-gnu/bits/dirent.h \
+ /usr/include/php/20151012/Zend/zend_constants.h \
+ /usr/include/php/20151012/main/php_reentrancy.h \
+ /usr/include/php/20151012/main/php.h \
+ /usr/include/php/20151012/Zend/zend_exceptions.h \
+ /usr/include/php/20151012/Zend/zend_interfaces.h \
+ /usr/include/php/20151012/Zend/zend_API.h \
+ /usr/include/php/20151012/Zend/zend_closures.h \
+ /usr/include/php/20151012/main/SAPI.h \
+ /usr/include/php/20151012/Zend/zend_llist.h \
+ /usr/include/php/20151012/Zend/zend_operators.h \
+ zend/../include/visibility.h zend/../include/deprecated.h \
+ zend/../include/noexcept.h zend/../include/thread_local.h \
+ zend/../include/platform.h zend/../include/version.h \
+ zend/../include/inivalue.h zend/../include/ini.h \
+ zend/../include/exception.h zend/../include/fatalerror.h \
+ zend/../include/streams.h zend/../include/type.h \
+ zend/../include/errors.h zend/../include/hashparent.h \
+ zend/../include/value.h zend/../include/zval.h \
+ zend/../include/valueiterator.h zend/../include/array.h \
+ zend/../include/object.h zend/../include/globals.h \
+ zend/../include/argument.h zend/../include/byval.h \
+ zend/../include/byref.h zend/../include/global.h \
+ zend/../include/hashmember.h zend/../include/super.h \
+ zend/../include/parameters.h zend/../include/modifiers.h \
+ zend/../include/base.h zend/../include/countable.h \
+ zend/../include/arrayaccess.h zend/../include/serializable.h \
+ zend/../include/iterator.h zend/../include/traversable.h \
+ zend/../include/classtype.h zend/../include/classbase.h \
+ zend/../include/interface.h zend/../include/constant.h \
+ zend/../include/zendcallable.h zend/../include/class.h \
+ zend/../include/namespace.h zend/../include/extension.h \
+ zend/../include/call.h zend/../include/script.h zend/../include/file.h \
+ zend/../include/function.h zend/../common/extensionbase.h \
+ zend/../common/streambuf.h zend/init.h zend/callable.h \
+ /usr/include/c++/5/cstring zend/nativefunction.h zend/method.h \
+ zend/member.h zend/nullmember.h zend/numericmember.h zend/boolmember.h \
+ zend/stringmember.h zend/floatmember.h zend/arithmetic.h \
+ zend/origexception.h zend/notimplemented.h zend/property.h \
+ zend/valueiteratorimpl.h zend/hashiterator.h zend/invaliditerator.h \
+ zend/traverseiterator.h zend/iteratorimpl.h zend/classimpl.h \
+ zend/objectimpl.h zend/parametersimpl.h zend/extensionimpl.h \
+ zend/compileroptions.h zend/executestate.h zend/opcodes.h zend/functor.h \
+ zend/constantimpl.h zend/delayedfree.h zend/extensionpath.h \
+ zend/symbol.h zend/module.h
